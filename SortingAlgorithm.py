ed3d0e336f741d2725253f6c161f6b2464265fa8
Omar Marouf <ooshiba02@gmail.com>
2025-12-25 20:07:58 +0200

    Added SortingAlgoInterface

---
+++ SortingAlgorithm.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from typing import Any, List
+class SortingAlgorithm(ABC):
+    def __init__(self) -> None:
+        self.name = self.__class__.__name__
+    
+    @abstractmethod
+    def sort(self, arr : List[int]) -> List[int]:
+        pass
+        
